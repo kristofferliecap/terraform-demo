bf5961960e1c0a869b9caeaa287c82f5a2d2aa30
Kristoffer Lie <kristoffer.lie@capgemini.com>
2024-12-18 09:17:02 +0100

    initielt oppsett av asp, app og key vault

---
+++ enkelt_oppsett/main.tf
@@ -0,0 +1,48 @@
+# Root resouce group
+resource "azurerm_resource_group" "root" {
+  name     = "root"
+  location = "West Europe"
+  tags = {
+    app       = "Mangekampen"
+    managedBy = "Terraform"
+  }
+}
+
+### Web app ### 
+resource "azurerm_service_plan" "demo-asp" {
+  name                = "demo-asp"
+  resource_group_name = azurerm_resource_group.root.name
+  location            = azurerm_resource_group.root.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+  tags = {
+    app       = "Mangekampen"
+    managedBy = "Terraform"
+  }
+}
+
+resource "azurerm_linux_web_app" "demo-app" {
+  name                = "demo-terraform-app"
+  resource_group_name = azurerm_resource_group.root.name
+  location            = azurerm_resource_group.root.location
+  service_plan_id     = azurerm_service_plan.demo-asp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "8.0"
+    }
+  }
+}
+
+## Key Vault ## 
+resource "azurerm_key_vault" "demo-kv" {
+  name                       = "demoappkeyvault2024"
+  location                   = azurerm_resource_group.root.location
+  resource_group_name        = azurerm_resource_group.root.name
+  tenant_id                  = ""
+  soft_delete_retention_days = 7
+
+  sku_name = "standard"
+
+  enable_rbac_authorization = true
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Demo av terraform for studenter på HiØ
+
+Studenter som skriver oppgave sammen med Capgemini
